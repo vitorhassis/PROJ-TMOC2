fd122d899671d24a422b39ddbd52708c558c215a
Vitor Assis <vitorassis997@gmail.com>
2024-08-27 15:38:12 -0300

    reta final do projeto

--- js/quiz.js
+++ js/quiz.js
@@ -24,6 +24,39 @@ function nextQuestion(e) {
     if (currentIndex < questions.length -1) {
         currentIndex++;
         loadQuestion();
-    } else finish();
+    } else {
+        finish();
+    } 
 }
 
+function finish() {
+    textFinish.innerHTML = `Você acertou ${questionCorrect} de ${question.length}`;
+    content.style.display = "none";
+    contentFinish.style.display = "flex";
+}
+
+function loadQuestion() {
+    questionqnt.innerHTML = `${currentIndex + 1}/${question.length}`;
+    const item = questions [currentIndex]; //obter a pergunta e sua resposta de acordo com o indice atual
+    answers.innerHTML = "";
+    question.innerHTML = item.question;
+
+    item.answers.forEach((answer) => {
+        const div = document.createElement("div");
+
+        div.innerHTML = `
+        <button class="answer" data-correct="${answer.correct}">
+          ${answer.option}
+        </button>
+        `;
+        //atributo data-correct é usado para armazenar a informação sobre se a resposta é correta ou não. Ou seja, quando o usuário clica em um botão, o código verifica o valor de data-correct para determinar se a resposta está correta (answer correct) dentre as opções de resposta (answer.option)
+
+        answers.appendChild(div);
+    });
+
+    document.querySelectorAll(".answer").forEach((item) => {
+        item.addEventListener("click", nextQuestion);
+    });
+}
+
+loadQuestion();
